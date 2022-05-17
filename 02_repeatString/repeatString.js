38313067de16ff4fe5bd7d9912d50080379c486e
Ron Rizzo <churchmouth13@gmail.com>
2022-05-17 10:19:50 -0400

    Add basic function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+function repeatString(string, num) {
+    let stringFinal = string;
+    numFix = parseInt(num);
+    for (i = numFix; i>1; i--) {
+        stringFinal =`${stringFinal}${string}`;
+    }
+    return stringFinal;
 };
 
 // Do not edit below this line
