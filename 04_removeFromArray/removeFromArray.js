51cfde621c9bed95bc1cd8a50c2384772fd54fe9
Ron Rizzo <churchmouth13@gmail.com>
2022-05-18 12:59:10 -0400

    Complete assignment
    I accidentally googled the answer on this one. To be fair, this was what
    I was trying to do, but its done using arrow functions which I'm not
    quite comfortable with. Going to study this deeper and make sure I
    actually understand how I solved this one.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(toReverse, ...theArgs) {
+    //use a for loop to iterate through theArgs
+    //check each ele in theArgs against toReverse
+    //for each ele:
+        //if = ele in toReverse, remove from toReverse
+        //if not present in toReverse, do nothing
+        //maybe make a function to check the values of theArgs
+    return toReverse.filter(x => !theArgs.includes(x));
 };
 
 // Do not edit below this line
