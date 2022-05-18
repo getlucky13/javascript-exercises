6ea2275796ee4fa8ee0137ce956a9929fe31db52
Ron Rizzo <churchmouth13@gmail.com>
2022-05-18 09:19:10 -0400

    Misc Fixs
    blarg

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,11 @@
-function repeatString(string, num) {
-    let stringFinal = string;
-    numFix = parseInt(num);
-    for (i = numFix; i>1; i--) {
-        stringFinal =`${stringFinal}${string}`;
+const repeatString = function(word, times) {
+    if (times < 0) return 'ERROR';
+    let string = '';
+    for (let i = 0; i < times; i++) {
+      string += word;
     }
-    return stringFinal;
-};
-
+    return string;
+  };
+  
 // Do not edit below this line
 module.exports = repeatString;
