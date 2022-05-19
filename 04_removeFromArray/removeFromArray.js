ee4f324422bce0a9a0266079c5287b275d1c1249
Ron Rizzo <churchmouth13@gmail.com>
2022-05-18 22:10:09 -0400

    Remove comments
    Removes comments from function. Problem complete.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,4 @@
 const removeFromArray = function(toReverse, ...theArgs) {
-    //use a for loop to iterate through theArgs
-    //check each ele in theArgs against toReverse
-    //for each ele:
-        //if = ele in toReverse, remove from toReverse
-        //if not present in toReverse, do nothing
-        //maybe make a function to check the values of theArgs
     return toReverse.filter(x => !theArgs.includes(x));
 };
 
