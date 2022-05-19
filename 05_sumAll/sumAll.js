ba834c74ba483974b0e31a431cb6db18bc180f7b
Ron Rizzo <churchmouth13@gmail.com>
2022-05-19 09:27:59 -0400

    Complete assignment
    Sorts array of args and uses the .every method of arrays to check each
    array for negative or non number vals and set a boolean for each for
    error handling. Using a for lops, iterates from the last val of theArgs
    to the first, adding them to the sum var, then returning the final sum.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let theArgs = [...arguments];
+    let theArgsSorted = theArgs.sort(function(a, b){return a - b});
+    let areNums = theArgsSorted.every(element => {return typeof element == 'number'});
+    let arePos = theArgsSorted.every(element => element > 0);
+    let sum = 0;
+    if (!areNums) return 'ERROR';
+    if (!arePos) return 'ERROR';
+    for (let i = theArgsSorted[1]; i >= theArgsSorted[0]; i--) {
+        sum += i; 
+    }
+    return sum;
+/*if (typeOf a != 'number' || typeOf b != 'number') {
 
+}*/
 };
 
 // Do not edit below this line
