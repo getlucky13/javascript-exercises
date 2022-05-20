ac588fd4b2df25f1e740fc6a80ba5680afe995df
Ron Rizzo <churchmouth13@gmail.com>
2022-05-20 10:22:00 -0400

    Added basic rounding function
    Added a function for rounding a # to a given precision, will be used to
    round results of temp conversion to first decimal point.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,9 @@
-const ftoc = function() {
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const ftoc = function(temp) {
 
 };
 
